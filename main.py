7c89d36e3f45266be15fbf09666cb08941ac7966
Arthur <draquo.drass@gmail.com>
2023-06-29 17:14:10 +0200

    Added a background and a delete button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json, os, pygame, shutil
+from btpygame import pygameimage, pygamebutton, collide, showtext
 
 
 home_directory = os.path.expanduser('~')
@@ -14,26 +15,27 @@ else:
     saveformat = "Random Speedrun #*"
 
 
-for (dirpath, dirnames, filenames) in os.walk(multipath + "/instances/"):
-    # Find all instances
-    for d in dirnames:
-        if d.startswith(instformat.replace("*", "")):
-            # Find all saves
-            print("Deleting saves in instance " + d)
-            for (dirpath, dirnames, filenames) in os.walk(multipath + "/instances/" + d + "/.minecraft/saves/"):
-                saveslist = []
-                for save in dirnames:
-                    if save.startswith(saveformat.replace("*", "")):
-                        saveslist.append(save)
-                for i in range(10):
-                    if len(saveslist) > 0:
-                        saveslist.pop(-1)
-                for subdir in saveslist:
-                    savepath = multipath + "/instances/" + d + "/.minecraft/saves/" + subdir
-                    shutil.rmtree(savepath)
-                break
-            print("Saves have been deleted in " + d + " with sucess")
-    break
+def deletesaves():
+    for (dirpath, dirnames, filenames) in os.walk(multipath + "/instances/"):
+        # Find all instances
+        for d in dirnames:
+            if d.startswith(instformat.replace("*", "")):
+                # Find all saves
+                print("Deleting saves in instance " + d)
+                for (dirpath, dirnames, filenames) in os.walk(multipath + "/instances/" + d + "/.minecraft/saves/"):
+                    saveslist = []
+                    for save in dirnames:
+                        if save.startswith(saveformat.replace("*", "")):
+                            saveslist.append(save)
+                    for i in range(10):
+                        if len(saveslist) > 0:
+                            saveslist.pop(-1)
+                    for subdir in saveslist:
+                        savepath = multipath + "/instances/" + d + "/.minecraft/saves/" + subdir
+                        shutil.rmtree(savepath)
+                    break
+                print("Saves have been deleted in " + d + " with sucess")
+        break
 
 pygame.init()
 screen = pygame.display.set_mode((600, 300))
@@ -41,11 +43,22 @@ pygame.display.set_caption('SavesCleaner by DraquoDrass')
 clock = pygame.time.Clock()
 running = True
 
+background = pygameimage(pygame.image.load("assets/background.png"), (0, 0))
+btn_delete = pygamebutton(pygame.transform.scale(pygame.image.load("assets/delete.png"), (200, 60)), pygame.transform.scale(pygame.image.load("assets/delete_t.png"), (200, 60)), (200, 230))
+
 while running:
+
+    screen.blit(background.image, background.pos)
+
+    btn_delete.display(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == pygame.BUTTON_LEFT:
+                if collide(btn_delete, event.pos):
+                    deletesaves()
 
     pygame.display.flip()
     clock.tick(60)
