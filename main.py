bae52af0c5f9a41988985d40a678a21af2587166
Arthur <draquo.drass@gmail.com>
2023-07-02 10:24:09 +0200

    Added options menu and some buttons

--- main.py
+++ main.py
@@ -4,18 +4,26 @@ from btpygame import pygameimage, pygamebutton, collide, showtext
 
 home_directory = os.path.expanduser('~')
 if os.path.isfile(home_directory + "/.savescleaner/options.json"):
+    optionschecked = True
+else:
+    f = open(home_directory + "/.savescleaner/options.json", "w")
+    f.writelines(["{\n",
+                  '\t"multipath": "your multimc path, ex: C:/MultiMC",\n',
+                  '\t"instformat": "instances name format, ex: RSG_*",\n',
+                  '\t"saveformat": "saves name format, ex: Random Speedrun #*"\n',
+                  '}'
+    ])
+    f.close()
+    optionschecked = False
+
+
+def deletesaves():
     f = open(home_directory + "/.savescleaner/options.json")
     data = json.load(f)
     multipath = data["multipath"]
     instformat = data["instformat"]
     saveformat = data["saveformat"]
-else:
-    multipath = home_directory + "/Documents/MultiMC"
-    instformat = "RSG_*"
-    saveformat = "Random Speedrun #*"
-
-
-def deletesaves():
+    f.close()
     for (dirpath, dirnames, filenames) in os.walk(multipath + "/instances/"):
         # Find all instances
         for d in dirnames:
@@ -38,27 +46,38 @@ def deletesaves():
         break
 
 pygame.init()
-screen = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((339, 380))
 pygame.display.set_caption('SavesCleaner by DraquoDrass')
 clock = pygame.time.Clock()
 running = True
 
 background = pygameimage(pygame.image.load("assets/background.png"), (0, 0))
-btn_delete = pygamebutton(pygame.transform.scale(pygame.image.load("assets/delete.png"), (200, 60)), pygame.transform.scale(pygame.image.load("assets/delete_t.png"), (200, 60)), (200, 230))
+btn_clean = pygamebutton(pygame.image.load("assets/clean.png"), pygame.image.load("assets/clean.png"), (50, 10))
+btn_saves = pygamebutton(pygame.image.load("assets/saves.png"), pygame.image.load("assets/saves_t.png"), (60, 80))
+btn_screens = pygamebutton(pygame.image.load("assets/screens.png"), pygame.image.load("assets/screens_t.png"), (14, 150))
+btn_logs = pygamebutton(pygame.image.load("assets/logs.png"), pygame.image.load("assets/logs_t.png"), (85, 220))
+btn_options = pygamebutton(pygame.image.load("assets/options.png"), pygame.image.load("assets/options_t.png"), (25, 310))
 
 while running:
 
     screen.blit(background.image, background.pos)
-
-    btn_delete.display(screen)
+    if optionschecked:
+        btn_clean.display(screen)
+        btn_saves.display(screen)
+        btn_screens.display(screen)
+        btn_logs.display(screen)
+    btn_options.display(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == pygame.BUTTON_LEFT:
-                if collide(btn_delete, event.pos):
+                if collide(btn_saves, event.pos):
                     deletesaves()
+                elif collide(btn_options, event.pos):
+                    os.startfile(home_directory + "/.savescleaner/options.json")
+                    optionschecked = True
 
     pygame.display.flip()
     clock.tick(60)
